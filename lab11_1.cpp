8610e96899309c18fbb79b8c987d4b5c5ea4d46d
klanpakwaenchananyoo <klanpakwaenchananyoo@gmail.com>
2025-12-26 14:42:07 +0700

    lab11_1.cpp

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,29 @@
-// cout << "Press Enter 3 times to reveal your future.";
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+    // แสดงข้อความเริ่มต้น
+    cout << "Press Enter 3 times to reveal your future." << endl;
+
+    // รับค่าการกด Enter 3 ครั้ง
+    for (int i = 0; i < 3; i++) {
+        cin.get();
+    }
+
+    // ตั้งค่า Seed สำหรับการสุ่มโดยใช้เวลาปัจจุบัน
+    srand(time(0));
+
+    // สุ่มตัวเลข 0-8 เพื่อเลือกเกรด
+    int randomIndex = rand() % 9;
+    string grades[] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+
+    // แสดงผลลัพธ์
+    cout << "You will get " << grades[randomIndex] << " in this 261102." << endl;
+
+    return 0;
+}// cout << "Press Enter 3 times to reveal your future.";
 // cout << "You will get A in this 261102.";
